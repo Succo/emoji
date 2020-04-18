fc3f1444532d035d0155dfe57f1238d4f8b35a02
Fabrice <fabrice.vaillant@centraliens.net>
2020-04-18 15:23:25 +0200

    Adds support for regional indicators

---
+++ regional_indicator.go
@@ -0,0 +1,9 @@
+package emoji
+
+import "unicode"
+
+var RegionalIndicator = &unicode.RangeTable{
+	R32: []unicode.Range32{
+		{Lo: uint32('🇦'), Hi: uint32('🇿')},
+	},
+}
---
+++ regional_indicator_test.go
@@ -0,0 +1,24 @@
+package emoji
+
+import (
+	"testing"
+	"unicode"
+	"unicode/utf8"
+
+	"github.com/Succo/countrymoji"
+)
+
+func RegionalIndicator_Test(t *testing.T) {
+	for _, c := range "abcdefghijklmnopqrstuvwxyz" {
+		r, _ := utf8.DecodeRuneInString(countrymoji.Alpha2ToFlag(string(c)))
+		if !unicode.Is(RegionalIndicator, r) {
+			t.Errorf("%q code %X is not counted as a regional indicator", r, r)
+		}
+	}
+
+	for _, c := range "abcxyz世界\nş123#. 😇🔁" {
+		if unicode.Is(RegionalIndicator, c) {
+			t.Errorf("%q code %X is not counted as a regional indicator", c, c)
+		}
+	}
+}
