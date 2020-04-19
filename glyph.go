463692e349637bec98d2e0b3ffbeaf01d4126211
Fabrice <fabrice.vaillant@centraliens.net>
2020-04-19 11:44:47 +0200

    Uses decode in PossibleGlyph

--- glyph.go
+++ glyph.go
@@ -34,69 +34,8 @@ const termTag = 0xE007F
 // There should not be false negative (glyph wrongly detected as an emoji)
 // There is false positive such inexistant flag indicator
 func PossibleGlyph(s string) bool {
-	r1, n1 := utf8.DecodeRuneInString(s)
-	if n1 == 0 {
-		return false
-	}
-	if unicode.Is(RegionalIndicator, r1) {
-		r2, n2 := utf8.DecodeRuneInString(s[n1:])
-		if n2 == 0 {
-			return false
-		}
-		return unicode.Is(RegionalIndicator, r2) && len(s) == n1+n2
-	}
-	n := n1
-	for unicode.Is(Emoji, r1) {
-		r2, n2 := utf8.DecodeRuneInString(s[n:])
-		if n2 == 0 {
-			return unicode.Is(ExtendedPictographic, r1)
-		}
-		n += n2
-
-		if r2 == emojiVS {
-			r3, n3 := utf8.DecodeRuneInString(s[n:])
-			if n3 == 0 {
-				return true
-			}
-			n += n3
-			if r3 == enclosingKeycap {
-				r2, n2 = utf8.DecodeRuneInString(s[n:])
-				if n2 == 0 {
-					return true
-				}
-				n += n2
-			} else {
-				r2, n2 = r3, n3
-			}
-		} else if unicode.Is(EmojiModifier, r2) && unicode.Is(EmojiModifierBase, r1) {
-			r2, n2 = utf8.DecodeRuneInString(s[n:])
-			if n2 == 0 {
-				return true
-			}
-			n += n2
-		} else if unicode.Is(Tag, r2) && r1 == '🏴' {
-			for unicode.Is(Tag, r2) {
-				r2, n2 = utf8.DecodeRuneInString(s[n:])
-				n += n2
-			}
-			if r2 != termTag {
-				return false
-			}
-			r2, n2 = utf8.DecodeRuneInString(s[n:])
-			if n2 == 0 {
-				return true
-			}
-			n += n2
-		}
-
-		if r2 != zeroWidthJoiner {
-			return false
-		}
-
-		r1, n1 = utf8.DecodeRuneInString(s[n:])
-		n += n1
-	}
-	return false
+	g, ok, _ := Decode(s)
+	return ok && g == s
 }
 
 // Decode returns
