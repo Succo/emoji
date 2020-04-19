bda7aa2bc73b39a9fccbd21347c8f8decdaa518b
Fabrice <fabrice.vaillant@centraliens.net>
2020-04-19 11:42:46 +0200

    Adds test parsing emoji with appended

--- glyph_test.go
+++ glyph_test.go
@@ -155,12 +155,23 @@ func Test_Decode(t *testing.T) {
 		if len(g) != n {
 			t.Errorf("Decode(%q) returned incoherent len", s)
 		}
-		g, ok, n = Decode(s + "a")
-		if PossibleGlyph(s + "a") {
-			t.Errorf("%q returned positive", s+"a")
-		}
-		if PossibleGlyph(s + s) {
-			t.Errorf("%q returned positive", s+s)
+	}
+	for _, s1 := range emojiTest {
+		for _, s2 := range append([]string{"aaa", "bbb"}, emojiTest...) {
+			s := s1 + s2
+			g, ok, n := Decode(s)
+			if !ok {
+				t.Errorf("Decode(%q) returned negative %q ", s, g)
+			}
+			if g != s1 {
+				t.Errorf("Decode(%q) returned not the full string but %q (%X != %X)", s, g, s1, g)
+			}
+			if len(g) != n {
+				t.Errorf("Decode(%q) returned incoherent len", s)
+			}
+			if s[n:] != s2 {
+				t.Errorf("Decode(%q) returned incoherent len full string but %q (%X != %X)", s2, s[n:], s2, s[n:])
+			}
 		}
 	}
 }
